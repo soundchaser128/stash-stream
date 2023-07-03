3625495084a5d49c14081ff739d2ea7b0b3e1a4c
soundchaser128 <soundchaser128@gmail.com>
2023-07-03 17:39:35 +0200

    start building some UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useQuery } from '@apollo/client'
-import { gql } from './__generated__'
-import { useState } from 'react'
+import {useQuery} from "@apollo/client"
+import {gql} from "./__generated__"
+import {useState} from "react"
+import {HiChevronDown, HiChevronUp} from "react-icons/hi2"
 
 const GET_SCENES = gql(`
 query GetScenes {
   findScenes {
   	scenes {
+      id
       title
-      sceneStreams {
-        url
-        label
-      }
       files {
         basename
       }
@@ -19,13 +17,44 @@ query GetScenes {
 }`)
 
 function App() {
-  const { data, loading } = useQuery(GET_SCENES, {})
+  const {data} = useQuery(GET_SCENES, {})
   const [currentSceneIndex, setCurrentSceneIndex] = useState(0)
+  const video = data?.findScenes.scenes[currentSceneIndex]
+  const length = data?.findScenes.scenes.length || 0
+  const streamUrl = `http://localhost:9999/scene/${video?.id}/stream`
 
   return (
     <main className="h-screen w-screen">
-      <div className="flex">
-        <video />
+      <div className="relative">
+        <div className="absolute flex flex-col gap-4 right-2 top-1/2 -translate-y-1/2 z-10">
+          <button
+            className="rounded-full bg-gray-200 p-2 bg-opacity-50 disabled:opacity-25"
+            disabled={length <= 1 || currentSceneIndex === 0}
+            onClick={() =>
+              setCurrentSceneIndex(
+                Math.max((currentSceneIndex - 1) % length, 0)
+              )
+            }
+          >
+            <HiChevronUp />
+          </button>
+
+          <button
+            className="rounded-full bg-gray-200 p-2 bg-opacity-50"
+            onClick={() =>
+              setCurrentSceneIndex((currentSceneIndex + 1) % length)
+            }
+          >
+            <HiChevronDown />
+          </button>
+        </div>
+        <video
+          className="w-screen h-screen object-cover"
+          src={streamUrl}
+          muted
+          autoPlay
+          loop
+        />
       </div>
     </main>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './main.css'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./main.css"
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: 'http://localhost:9999/graphql',
+  uri: "http://localhost:9999/graphql",
   cache: new InMemoryCache(),
-});
+})
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )

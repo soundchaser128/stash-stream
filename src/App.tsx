4692a65e2b1714f9b8fdcf2b635a2c2cc04bc4fe
soundchaser128 <soundchaser128@gmail.com>
2023-07-03 19:43:06 +0200

    some more work

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,41 @@ query GetScenes {
   }
 }`)
 
+function NavButtons({
+  currentSceneIndex,
+  goToPreviousVideo,
+  goToNextVideo,
+}: {
+  currentSceneIndex: number
+  goToPreviousVideo: (position?: number) => void
+  goToNextVideo: (position?: number) => void
+}) {
+  return (
+    <div className="absolute flex flex-col gap-4 right-2 top-1/2 -translate-y-1/2 z-10">
+      <button
+        className="rounded-full bg-gray-200 p-2 bg-opacity-50 disabled:opacity-25"
+        disabled={length <= 1 || currentSceneIndex === 0}
+        onClick={() => goToPreviousVideo(0)}
+      >
+        <HiChevronUp />
+      </button>
+
+      <button
+        className="rounded-full bg-gray-200 p-2 bg-opacity-50"
+        onClick={() => goToNextVideo(0)}
+      >
+        <HiChevronDown />
+      </button>
+    </div>
+  )
+}
+
 function App() {
   const {data} = useQuery(GET_SCENES, {})
   const [currentSceneIndex, setCurrentSceneIndex] = useState(0)
   const video = data?.findScenes.scenes[currentSceneIndex]
   const length = data?.findScenes.scenes.length || 0
   const streamUrl = `http://localhost:9999/scene/${video?.id}/stream`
-  const [showVideo, setShowVideo] = useState(false)
   const [position, setPosition] = useState(0)
   const previousVideo = data?.findScenes.scenes[currentSceneIndex - 1]
   const nextVideo = data?.findScenes.scenes[currentSceneIndex + 1]
@@ -33,21 +61,30 @@ function App() {
   const nextStreamUrl = `http://localhost:9999/scene/${nextVideo?.id}/stream`
 
   const bind = useGesture({
-    onDrag: ({delta: [dx, dy]}) => {
-      setPosition((p) => p + dy)
+    onDrag: ({delta: [, dy]}) => {
+      setPosition((p) => Math.min(0, p + dy))
+      if (Math.abs(position + dy) > window.innerHeight) {
+        goToNextVideo(-position)
+      }
     },
   })
 
-  const goToNextVideo = () => {
+  const goToNextVideo = (position?: number) => {
     setCurrentSceneIndex((currentSceneIndex + 1) % length)
+    if (typeof position !== "undefined") {
+      setPosition(position)
+    }
   }
 
-  const goToPreviousVideo = () => {
+  const goToPreviousVideo = (position?: number) => {
     setCurrentSceneIndex(Math.max((currentSceneIndex - 1) % length, 0))
+    if (typeof position !== "undefined") {
+      setPosition(position)
+    }
   }
 
-  useHotkeys("up", goToPreviousVideo, [currentSceneIndex, length])
-  useHotkeys("down", goToNextVideo, [currentSceneIndex, length])
+  useHotkeys("w", () => goToPreviousVideo(0), [currentSceneIndex, length])
+  useHotkeys("s", () => goToNextVideo(0), [currentSceneIndex, length])
 
   const onEnded = () => {
     setCurrentSceneIndex((currentSceneIndex + 1) % length)
@@ -55,37 +92,29 @@ function App() {
 
   return (
     <main className="h-screen w-screen">
-      <div className="relative">
-        <div className="absolute top-4 left-4 z-10 text-white text-xl">
+      <div className="relative h-full w-full">
+        <div className="absolute top-4 left-4 z-10 text-white text-4xl">
           Stash
         </div>
 
-        <div className="absolute flex flex-col gap-4 right-2 top-1/2 -translate-y-1/2 z-10">
-          <button
-            className="rounded-full bg-gray-200 p-2 bg-opacity-50 disabled:opacity-25"
-            disabled={length <= 1 || currentSceneIndex === 0}
-            onClick={() =>
-              setCurrentSceneIndex(
-                Math.max((currentSceneIndex - 1) % length, 0)
-              )
-            }
-          >
-            <HiChevronUp />
-          </button>
-
-          <button
-            className="rounded-full bg-gray-200 p-2 bg-opacity-50"
-            onClick={() =>
-              setCurrentSceneIndex((currentSceneIndex + 1) % length)
-            }
-          >
-            <HiChevronDown />
-          </button>
+        <div className="">
+          <div className="absolute bottom-4 left-1/2 z-10 -translate-x-1/2">
+            <div className="flex flex-row gap-1 truncate text-white text-4xl text-shadow-lg">
+              {video?.title || video?.files[0].basename}
+            </div>
+          </div>
         </div>
-        {position > 0 && currentSceneIndex - 1 > 0 && (
+
+        <NavButtons
+          currentSceneIndex={currentSceneIndex}
+          goToNextVideo={goToNextVideo}
+          goToPreviousVideo={goToPreviousVideo}
+        />
+
+        <div className="touch-none" {...bind()}>
           <div className="absolute top-0 left-0 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center">
             <animated.video
-              className="w-screen h-screen object-cover absolute top-0 left-0 touch-none"
+              className="w-screen h-screen object-cover absolute top-0 left-0"
               src={previousStreamUrl}
               muted
               autoPlay
@@ -94,23 +123,21 @@ function App() {
               }}
             />
           </div>
-        )}
-        <animated.video
-          {...bind()}
-          className="w-screen h-screen object-cover absolute top-0 left-0 touch-none"
-          src={streamUrl}
-          muted
-          autoPlay
-          onEnded={onEnded}
-          style={{
-            top: position,
-          }}
-        />
 
-        {position < 0 && (
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center">
+          <animated.video
+            className="w-screen h-screen object-cover absolute top-0 left-0"
+            src={streamUrl}
+            muted
+            autoPlay
+            onEnded={onEnded}
+            style={{
+              top: position,
+            }}
+          />
+
+          <div className="absolute top-0 left-0 w-screen h-screen">
             <animated.video
-              className="w-screen h-screen object-cover absolute top-0 left-0 touch-none"
+              className="w-screen h-screen object-cover absolute top-0 left-0"
               src={nextStreamUrl}
               muted
               autoPlay
@@ -119,7 +146,7 @@ function App() {
               }}
             />
           </div>
-        )}
+        </div>
       </div>
     </main>
   )
